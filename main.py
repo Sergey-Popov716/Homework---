120a6c95c14afd8967ba268595bdb769e5a5e785
Sergey-Popov716 <gazbanda5@gmail.com>
2024-09-25 16:57:01 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,6 @@
+# ответ на практическое задание по теме "Переменные".
+Course_name = 'Python'
+Number_of_homework = 12
+Number_of_hours = 1.5
+Time_for_one_homework = Number_of_hours / Number_of_homework
+print(Course_name, ', всего задач:' ,Number_of_homework, ', затрачено часов:' ,Number_of_hours, ', среднее время выполнения' ,Time_for_one_homework,'часа')
